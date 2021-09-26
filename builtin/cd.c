a07b55aeb5ebb0e09b9438713d8b398fc006ed9e
minchoi <minchoi@student.42seoul.kr>
2021-09-26 17:27:48 +0900

    not change

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: minchoi <minchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 11:55:04 by minchoi           #+#    #+#             */
-/*   Updated: 2021/09/26 16:32:01 by minchoi          ###   ########.fr       */
+/*   Updated: 2021/09/26 16:40:33 by minchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	cd_home(char **ins, char *path)
 {
+	ins = NULL;
+	path = NULL;
 	return (0);
 }
 
@@ -24,7 +26,7 @@ void	ft_cd(char **ins)
 
 	path = ins[1];
 	if (ins[1] == NULL || ft_strncmp(path, "~", ft_strlen(path)) == 0)
-		ret == cd_home(ins, path);
+		ret = cd_home(ins, path);
 	//else if (ins[1][0] == '$') //경로가 환경변수 일 때
 	else
 	{
--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: minchoi <minchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 16:41:40 by hynam             #+#    #+#             */
-/*   Updated: 2021/09/26 15:11:42 by minchoi          ###   ########.fr       */
+/*   Updated: 2021/09/26 16:39:53 by minchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[], char **envp)
 {
 	int		status;
 	char	*str;
@@ -20,15 +20,20 @@ int	main(void)
 	char	**ins;
 	int		i;
 	char	*buf;
-	size_t	size;
-	size = 200;
 	buf = NULL;
+	argc = 0;
+	argv = NULL;
+	while (envp[argc])
+	{
+		printf("%s\n", envp[argc]);
+		argc++;
+	}
 
 	str = NULL;
 	status = 0;
 	while (1)
 	{
-		printf("%s ", getcwd(buf, size));
+		printf("%s ", getcwd(buf, 1024));
 		str = readline("> ");
 		ins = ft_split(str, ' '); // Parsing 대용
 		if (ft_strncmp(ins[0], "exit", ft_strlen(ins[0])) == 0)
